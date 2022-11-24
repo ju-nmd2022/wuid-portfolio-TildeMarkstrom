fad1bc55e1f950cf48c1773d096897ad4f9c005f
TildeMarkstrom <tilde_markstrom@hotmail.com>
2022-11-24 01:20:21 +0100

    new storylines

--- Javascript Assign/game.js
+++ Javascript Assign/game.js
@@ -32,6 +32,7 @@ bagPic.addEventListener('click', function(){
     bagText.innerText=("1x");
     bagPic.style.visibility="hidden";
     rollDice.style.visibility="visible";
+    story.innerHTML=("Great! you found the bag! But who is that? It's one of the thieves and he challenges you in a round of dice for the bag. Roll the dice!")
 })
 
 //DICE
@@ -47,11 +48,11 @@ let myDices = Math.floor(Math.random()*6)+1;
 let hisDices = Math.floor(Math.random()*6)+1;
 if(myDices > hisDices){
     console.log("you win!");
-    alert("You won");
+    alert("Congrats! You beat the thief with rolling your dice higer then he! You won the game!");
 }else if (myDices == hisDices){
     console.log("tie, go again.");
     alert("tie, go again");
 }else{
     console.log("you lose!");
-    alert("You lost");
+    alert("Aw, you lost against the thief, he took your bag and you go home empty handed... The End.");
 }}
