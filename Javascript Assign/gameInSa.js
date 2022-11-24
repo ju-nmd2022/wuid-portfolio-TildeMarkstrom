685a93a67bcb50546214a81fe83c55eeb0ce66c5
TildeMarkstrom <mati22yl@student.ju.se>
2022-11-24 16:07:35 +0100

    final game

--- Javascript Assign/gameInSa.js
+++ Javascript Assign/gameInSa.js
@@ -13,7 +13,7 @@ let rollDice = document.getElementById("dices");
 //whiskey button
 whiskeyButton.addEventListener('click', function(){
     whiskeyText.innerText=("1x");
-    story.innerHTML=("The cowboy says that the abandoned saloon might be one of the hideouts for the thieves. He gives you a flashlight.");
+    story.innerHTML=("The cowboy says that the abandoned saloon might be one of the hideouts for the thieves. He gives you a flashlight to be able to see inside of the building.");
     flash.style.visibility="visible";
     whiskeyButton.style.visibility="hidden";
     sessionStorage.setItem("afterWhiskey", "AW");
@@ -22,7 +22,7 @@ whiskeyButton.addEventListener('click', function(){
 //flash button
 if(sessionStorage.getItem("afterWhiskey") !=null){
     whiskeyText.innerText=("1x");
-    story.innerHTML=("The cowboy says that the abandoned saloon might be one of the hideouts for the thieves. He gives you a flashlight.");
+    story.innerHTML=("The cowboy says that the abandoned saloon might be one of the hideouts for the thieves. He gives you a flashlight to be able to see inside of the building.");
     flash.style.visibility="visible";
     whiskeyButton.style.visibility="hidden";
 }
@@ -37,36 +37,3 @@ if(sessionStorage.getItem("afterFlash")!=null){
     flashlightText.innerText=("1x");    
     flash.remove();
 }
-/*
-
-
-//bag button
-bagPic.addEventListener('click', function(){
-    bagText.innerText=("1x");
-    bagPic.style.visibility="hidden";
-    rollDice.style.visibility="visible";
-    story.innerHTML=("Great! you found the bag! But who is that? It's one of the thieves and he challenges you in a round of dice for the bag. Roll the dice!")
-    isBagPickedUp = true;
-    sessionStorage.setItem("afterBag", "AB");
-})
-
-//DICE
-let myDices = Math.floor(Math.random()*6)+1;
-let hisDices = Math.floor(Math.random()*6)+1;
-    
-console.log("yours " + myDices);
-console.log("his " + hisDices);
-
-function rolldice(){
-let myDices = Math.floor(Math.random()*6)+1;
-let hisDices = Math.floor(Math.random()*6)+1;
-if(myDices > hisDices){
-    console.log("you win!");
-    alert("Congrats! You beat the thief with rolling your dice higer then he! You won the game!");
-}else if (myDices == hisDices){
-    console.log("tie, go again.");
-    alert("tie, go again");
-}else{
-    console.log("you lose!");
-    alert("Aw, you lost against the thief, he took your bag and you go home empty handed... The End.");
-}} */
--- Javascript Assign/gameOuSa.js
+++ Javascript Assign/gameOuSa.js
@@ -10,7 +10,7 @@ let rollDice = document.getElementById("dices");
 
 if(sessionStorage.getItem("afterWhiskey") !=null){
     whiskeyText.innerText=("1x");
-    story.innerHTML=("The cowboy says that the abandoned saloon might be one of the hideouts for the thieves. He gives you a flashlight.");
+    story.innerHTML=("The cowboy says that the abandoned saloon might be one of the hideouts for the thieves. He gives you a flashlight to be able to see inside of the building.");
 }
 
 if(sessionStorage.getItem("afterFlash")!=null){
